c11e8627e7e90dac6f5996efebc7ed2e1d727d30
Evan Kriminger <evankriminger@gmail.com>
2018-04-21 00:19:11 -0700

    building train function

--- train.py
+++ train.py
@@ -6,6 +6,9 @@ import os
 import glob
 import numpy as np
 import tensorflow as tf
+
+from models import generator, \
+                   discriminator
  
 
 HEIGHT = 64
@@ -13,7 +16,7 @@ WIDTH = 64
 CHANNELS = 3
 
 
-class PredictionBuffer:
+class ImageBuffer:
   '''Generated images are added to this buffer
   The discriminator trains on a mix of freshly generated images
   and images from this buffer
@@ -25,34 +28,39 @@ class PredictionBuffer:
   
   def add(self, x):
     if self.cnt < self.buffer_sz:
-      self.buffer[self.cnt:min(self.cnt+x.shape[0], self.buffer_sz)] = x
+      n_to_add = min(self.buffer_sz-self.cnt, x.shape[0])
+      self.buffer[self.cnt:self.cnt+n_to_add] = x[:n_to_add]
+      self.cnt += n_to_add
     else:
-       
-      
+      idx = np.random.choice(self.buffer_sz, x.shape[0], replace=False).sort()
+      self.buffer[idx] = x
 
   def sample(self, batch_size):
     idx = np.random.choice(self.cnt, batch_size, replace=True)
     return self.buffer[idx]
 
 
-def train(epochs=10, dis_buffer=32768):
-  x = tf.placeholder(tf.float32, (None, 218, 178, 3))
-  y = tf.placeholder(tf.float32, (None, 10))
+def train(epochs=10, dim_z=64, buffer_sz=32768):
+  z_ph = tf.placeholder(tf.float32, (None, dim_z))
+  dis_target_ph = tf.placeholder(tf.float32, (None, 1))
   training_ph = tf.placeholder(tf.bool, ())
   
   input_fn = CelebAInput().input_fn_factory(
     mode='train', 
     batch_size=2)
 
-  image = input_fn()  
-  output = model_test(image, mode='train')
+  image_tens = input_fn()  
+  dis_logits = discriminator(image_tens, training_ph, dim_h=dim_h)
+  gen_image = generator(z_ph, training_ph, dim_h=dim_h)
 
-  tf.Session(
+  old_images = ImageBuffer(buffer_sz=buffer_sz)
+
   with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    #for i_epoch in range(epochs):
     cnt = 0
-    while True:
+    for i_epoch in range(epochs):
+       
+      sess.run()
       print(sess.run(output).shape)
       print(cnt)
       cnt += 1
