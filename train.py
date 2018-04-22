b1b152491635e399979a211846402c1c1e054415
Evan Kriminger <evankriminger@gmail.com>
2018-04-21 23:42:34 -0700

    working on shuffle buffer

--- train.py
+++ train.py
@@ -40,27 +40,46 @@ class ImageBuffer:
     return self.buffer[idx]
 
 
-def train(epochs=10, dim_z=64, buffer_sz=32768):
+def train(batch_size=256, epochs=10, dim_z=64, buffer_sz=32768):
+  # placeholders
   z_ph = tf.placeholder(tf.float32, (None, dim_z))
   dis_target_ph = tf.placeholder(tf.float32, (None, 1))
   training_ph = tf.placeholder(tf.bool, ())
   
+  # build the input
   input_fn = CelebAInput().input_fn_factory(
     mode='train', 
     batch_size=2)
 
+  # input and output tensors
   image_tens = input_fn()  
   dis_logits = discriminator(image_tens, training_ph, dim_h=dim_h)
   gen_image = generator(z_ph, training_ph, dim_h=dim_h)
 
+  # buffer of generated images
   old_images = ImageBuffer(buffer_sz=buffer_sz)
 
   with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     cnt = 0
     for i_epoch in range(epochs):
-       
-      sess.run()
+      
       print(sess.run(output).shape)
       print(cnt)
       cnt += 1
+
+if __name__=='__main__':
+
+  x = tf.constant((np.arange(10)[:,None]+np.zeros((1,3))).astype(np.float32))
+  dataset = tf.data.Dataset.from_tensor_slices(x)
+  dataset = dataset.shuffle(buffer_size=3).repeat().batch(3)
+  iterator = dataset.make_initializable_iterator()
+  next_element = iterator.get_next()
+  with tf.Session() as sess:
+    sess.run(iterator.initializer)
+    while True:
+      try:
+        x = sess.run(next_element) 
+        print(x)
+      except tf.errors.OutOfRangeError:
+        break
