983ce21c93fb5db90867668c0154d1e445111100
Evan Kriminger <evankriminger@gmail.com>
2018-04-23 18:54:17 -0700

    adding scopes for discriminator reuse

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ import numpy as np
 import tensorflow as tf
 
 from data_pipeline import CelebAInput
-from models import erator, \
+from models import generator, \
                    discriminator
  
 
@@ -60,25 +60,31 @@ def train(batch_size=256, epochs=10, dim_z=128, buffer_sz=32768):
     epochs=epochs)
 
   # discriminator, generator, and generator score
-  D_logits = discriminator(image_tens, training_ph, dim_h=dim_h)
-  G_image = generator(z_ph, training_ph, dim_h=dim_h)
-  D_fake = discriminator(G_image)
+  with tf.variable_scope('models') as scope:
+    D_logits = discriminator(image_tens, training_ph, dim_h=dim_h)
+    G_image = generator(z_ph, training_ph, dim_h=dim_h)
+    scope.reuse_variables()
+    D_fake = discriminator(G_image, training_ph, dim_h=dim_h)
 
   # prepare losses
-  D_loss = tf.nn.softplu-
+  #D_loss = tf.nn.softplu-
   G_loss = tf.reduce_mean(tf.square(D_fake))
 
   # buffer of erated images
   old_images = ImageBuffer(buffer_sz=buffer_sz)
 
+  vars_to_train_dis = np.setdiff1d(
+    tf.trainable_variables(),
+    tf.trainable_variables('models/generator'))
+
   with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     sess.run(iterator.initializer)
     cnt = 0 
-    while True:
+    while False:
       try:
         #x = sess.run(D_logits, feed_dict={training_ph:False})
-        x = sess.run(_image, feed_dict={z_ph: np.random.randn(batch_size, dim_z).astype(np.float32), training_ph:False})
+        #x = sess.run(, feed_dict={z_ph: np.random.randn(batch_size, dim_z).astype(np.float32), training_ph:False})
         print(x.shape)
         cnt += x.shape[0]
         print(cnt)
