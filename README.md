e0fe68524a89c1f5f29f71638d1eb0eba0207870
Evan Kriminger <evankriminger@gmail.com>
2018-06-23 00:40:50 -0700

    moving tfrecord converter

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # bgan-tf
 Implementing the Boundary-Seeking GAN in TensorFlow. Credit to the authors of the [original paper](https://arxiv.org/abs/1702.08431v2), who also released their [Theano implementation](https://github.com/rdevon/BGAN).
 
+#### Usage
+
+First, download the celebrity faces (CelebA) dataset, put it in `data/`, and unzip it such that we have `data/img_align_celeba/`.
+
+Then prepare the tfrecords:
+
+```
+python make_tfrecords.py
+```
+
 Set training params in train.py and run training with:
+
 ```
 python train.py
 ```
--- make_tfrecords.py
+++ make_tfrecords.py
@@ -1,5 +1,7 @@
 import struct
 import os
+import urllib
+import zipfile
 import pickle
 import PIL
 import numpy as np
@@ -11,13 +13,16 @@ def jpg_reader(filename):
   return np.asarray(img)
 
 
-if __name__=="__main__":
+def to_tfrecords():
+  img_dir = 'data/img_align_celeba'
   
-  img_dir = 'img_align_celeba'
-  assert os.path.isdir(img_dir), 'Download the aligned image celeba zip'
+  if not os.path.isdir('data'):
+    os.mkdir('data')
+  
+  assert os.path.isdir(img_dir), 'Download the aligned image celeba .zip and unzip into the data/ directory'
   
   input_files = [os.path.join(img_dir, fn) for fn in os.listdir(img_dir) if fn.endswith(".jpg")]
-  output_file = 'img_align_celeba.tfrecords'
+  output_file = 'data/img_align_celeba.tfrecords'
 
   with tf.python_io.TFRecordWriter(output_file) as record_writer:
     for input_file in input_files:
@@ -29,3 +34,7 @@ if __name__=="__main__":
             "image": tf.train.Feature(bytes_list=tf.train.BytesList(value=[img.tobytes()]))}))
 
       record_writer.write(example.SerializeToString())
+
+
+if __name__ == '__main__':
+  to_tfrecords()
